75fb8ddb2ab55c15291125afaf83c16779c2f5b2
Ayana Bando <ayanaflax@yahoo.com>
2018-04-15 01:50:12 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -7,11 +7,12 @@ def key_for_min_value(name_hash)
   name_hash.collect do |name, value|
     value.each do |a,b|
       a <=> b
-    if value[0] < value[1] && value[2]
-       puts name
-    elsif name_hash.empty?
-      puts nil
+      if value[0] < value[1] && value[2]
+        puts name
+      elsif name_hash.empty?
+        puts nil
       #binding.pry
     end
+  end
   end
 end
